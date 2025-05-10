4fade9e3d6c1b61ce16bad87725831532b52bbc6
JERICHO URBANO <119677296+shiehb@users.noreply.github.com>
2025-05-10 15:03:55 +0800

    Update api_url.js

--- frontend/src/api/api_url.js
+++ frontend/src/api/api_url.js
@@ -19,7 +19,7 @@ const ensureTrailingSlash = (url) => {
 
 // Create axios instance
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api',
+  baseURL: 'https://k-to-drinks.onrender.com/api',
   headers: {
     'Accept': 'application/json',
     'Content-Type': 'application/json'
